897116c50a09175fd689744554b4f497f90551ee
Taerithroen <yura_plakhov@mail.ru>
2025-09-04 19:02:49 +0300

    create registration

--- client/src/entities/user/UserService.js
+++ client/src/entities/user/UserService.js
@@ -7,14 +7,29 @@ export class UserService {
   }
 
   static async signUp(userData) {
-    const response = await axiosInstance.post('/auth/signUp', userData);
-    console.log('response', response);
-    return response.data;
+    console.log('Sending signUp request to:', axiosInstance.defaults.baseURL + '/auth/signUp');
+    console.log('Request data:', userData);
+    
+    try {
+      const response = await axiosInstance.post('/auth/signUp', userData);
+      console.log('SignUp response:', response);
+      return response.data;
+    } catch (error) {
+      console.error('SignUp error:', error);
+      console.error('Error response:', error.response);
+      throw error;
+    }
   }
 
   static async signIn(userData) {
-    const response = await axiosInstance.post('/auth/signIn', userData);
-    return response.data;
+    console.log('Sending signIn request to:', axiosInstance.defaults.baseURL + '/auth/signIn');
+    try {
+      const response = await axiosInstance.post('/auth/signIn', userData);
+      return response.data;
+    } catch (error) {
+      console.error('SignIn error:', error);
+      throw error;
+    }
   }
 
   static async signOut() {
